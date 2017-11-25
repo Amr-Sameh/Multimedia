4639c1be9ee0aabfd4eaba69bbce8817504fa38c
Amr-Sameh <amrsameh.develop@gmail.com>
2017-11-25 20:48:05 +0200

    init api server & data acess

---
+++ Server/Server/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Server.Controllers
+{
+    public class UserController : ApiController
+    {
+
+
+        [HttpPost()]
+        [ActionName("login")]
+        public HttpResponseMessage login([FromBody]String username ,  String password) {
+
+            using (multimediaEntities entities = new multimediaEntities ()) {
+                user user = entities.users.ToList().Find(u => u.username == username);
+                if (user == null || user.password != password)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "User Not Found");
+
+                return Request.CreateResponse(HttpStatusCode.OK, user);
+                  
+                
+            }
+               
+        }
+
+    }
+}
